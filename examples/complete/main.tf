4d23dbbed0f5d7a708920d31e155a35c2cc4afa7
shanye <shanye.xzq@alibaba-inc.com>
2024-05-31 15:24:36 +0800

    examples/complete: improve the examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,3 +1,12 @@
+provider "alicloud" {
+  region = "cn-hangzhou"
+}
+
+resource "random_integer" "default" {
+  min = 10000
+  max = 99999
+}
+
 data "alicloud_dfs_zones" "default" {}
 
 variable "name" {
@@ -17,19 +26,24 @@ resource "alicloud_vswitch" "default" {
 }
 
 module "example" {
-  create                        = true
-  source                        = "../.."
-  storage_type                  = data.alicloud_dfs_zones.default.zones.0.options.0.storage_type
-  zone_id                       = data.alicloud_dfs_zones.default.zones.0.zone_id
+  create = true
+  source = "../.."
+
+  # alicloud_dfs_file_system
+  storage_type     = data.alicloud_dfs_zones.default.zones.0.options.0.storage_type
+  zone_id          = data.alicloud_dfs_zones.default.zones.0.zone_id
+  hdfs_name        = "tf_hdfs_name"
+  space_capacity   = 1024
+  hdfs_description = "The description of DFS file system."
+  # alicloud_dfs_access_group
+  access_group_name             = "tf_access_group_name_${random_integer.default.result}"
+  hdfs_access_group_description = "The description of DFS access group."
+  # alicloud_dfs_access_rule
+  network_segment             = "192.0.2.0/24"
+  dfs_access_rule_description = "The description of DFS access group."
+  priority                    = 10
+  # alicloud_dfs_mount_point
   vpc_id                        = alicloud_vpc.default.id
   vswitch_id                    = alicloud_vswitch.default.id
-  hdfs_name                     = "tf_hdfs_name"
-  space_capacity                = 1024
-  access_group_name             = "tf_access_group_name"
-  hdfs_access_group_description = "The description of DFS access group."
-  hdfs_description              = "The description of DFS file system."
-  network_segment               = "192.0.2.0/24"
-  dfs_access_rule_description   = "The description of DFS access group."
-  priority                      = 10
   alicloud_dfs_mount_point_name = "tf_mount_point_name"
 }
